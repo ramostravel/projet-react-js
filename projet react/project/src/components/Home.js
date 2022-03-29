70230d15c25d9093752af5407792ed37b68d17bb
ramostravel <medramzi98@gmail.com>
2022-03-29 22:29:43 +0200

    first commit

---
+++ projet react/project/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from "./Form"
+import NavBar from"./NavBar"
+function Home() {
+  return (
+    <div>
+        
+        <Form/>
+        <NavBar/>
+    </div>
+  )
+}
+
+export default Home
