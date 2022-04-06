efe5e9fc322c41379ce1e0a0b05527d5889d1219
ramostravel <medramzi98@gmail.com>
2022-04-06 15:47:06 +0200

    first commit

--- projet react/project/src/App.js
+++ projet react/project/src/App.js
@@ -1,15 +1,26 @@
 
 import './App.css';
-import Home from './components/Home'
-import Form from "./components/Form";
-
+import myIMG from "./imageInSrc.jpg"
+import myVideo from "./myVideo.mp4"
 
 function App() {
   return (
-    <div>
+  <div>
 
-<Home/>
+    <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+    <h1 className="title red">Your name here</h1>
+    <br/>
+    <img src={myIMG}  alt="src" />
+    <br/>
+    <img src="/imageInPublic.jpg" alt="imagePublic" />
     </div>
+      <video width="320" height="240" controls>
+
+      <source src={myVideo} type="video/mp4" />
+
+      </video>
+</div>
   );
 }
 
